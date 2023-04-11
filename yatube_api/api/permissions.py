c09616c685af5be578859708a764c44af3db5e08
borisovi <borisovi@krasrm.com>
2023-04-11 19:11:19 +0700

    2

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,13 +5,9 @@ class OwnerOrReadOnly(permissions.BasePermission):
     """Изменение доступно только автору."""
 
     def has_permission(self, request, view):
-        return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
-            )
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return (
-                request.method in permissions.SAFE_METHODS
-                or obj.author == request.user
-        )
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
